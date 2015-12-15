f5269a7ff4822ee9c62315c870666ab1dd308082
Karl Cartlidge <code@kcartlidge.com>
2015-12-15 00:03:40 +0000

    Added test for parameter dependency resolution.

--- tests/index-spec.js
+++ tests/index-spec.js
@@ -1,7 +1,13 @@
 var assert = require('assert');
 var IOC = require('../index');
 
-var stubModule = { hello: 'world' };
+var stubMessageProvider = { greeting: "Hello" };
+var stubModule = {
+	okay: true,
+	test: function (name, messageProvider) {
+		return messageProvider.greeting + " " + name;
+	}
+};
 
 describe("The NoDice IOC object", function () {
 	it("should exist globally", function () {
@@ -13,24 +19,28 @@ describe("The NoDice IOC object", function () {
 
 	describe("with a registered module", function(){
 		beforeEach(function(){
+			IOC.register('messageProvider', stubMessageProvider);
 			IOC.register('testModule', stubModule);
 		});
 
 		it("should add that module to the list of registrations", function () {
 			var registrations = IOC.getRegistrations();
-			assert(registrations[0] === 'testModule');
+			assert(registrations[1] === 'testModule');
 		});
 		it("should not have added a duplicate module (by name) to the registrations", function () {
 			IOC.register('testModule', stubModule);
 			var registrations = IOC.getRegistrations();
-			assert(registrations.length === 1);
+			assert(registrations.length === 2);
 		});
 		it("should be able to resolve the module", function () {
 			var resolution = IOC.resolve('testModule');
 			assert(typeof(resolution) === 'object');
 		});
 		it("should return the specific module requested", function () {
-			assert(IOC.resolve('testModule').hello === 'world');
+			assert(IOC.resolve('testModule').okay);
+		});
+		it("should inject dependencies into the module", function () {
+			assert(IOC.resolve('testModule').test('Bob') === "Hello Bob");
 		});
 	});
 });
